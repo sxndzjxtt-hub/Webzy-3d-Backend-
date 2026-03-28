52d79c024a7d625c181a641bc83f82685ba1ba70
sxndzjxtt-hub <sxndzjxtt@gmail.com>
2026-03-28 05:49:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -47,4 +47,5 @@ app.get("/", (req, res) => {
   res.send("Webzy backend running 🚀");
 });
 
-app.listen(3000, () => console.log("Server running"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running"));
